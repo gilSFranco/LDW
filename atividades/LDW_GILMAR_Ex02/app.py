70fa5e7df79162126cabd70db732939fbbbba6e1
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-09-12 20:39:01 -0300

    feat: some improvements made in code

--- atividades/LDW_GILMAR_Ex02/app.py
+++ atividades/LDW_GILMAR_Ex02/app.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     with app.test_request_context():
         db.create_all()
     
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
